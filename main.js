5a65d0c85972f02af5afd3a10e93d1aa94139ef1
Jorden <terrazas.jorden9@gmail.com>
2017-09-15 09:53:53 -0500

    george is a gaf

--- main.js
+++ main.js
@@ -16,8 +16,9 @@ function createWindow () {
   mainWindow = new BrowserWindow({
 	width: 420,
 	height: 420,
-	resizeable: false,
-	frame: false,
+	movable: false,
+	resizable: false,
+	frame: true,
 	center: false,
 	alwaysOnTop: true,
 	transparent: true,
@@ -53,7 +54,7 @@ app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
@@ -61,7 +62,7 @@ app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
 })
 
--- renderer.js
+++ renderer.js
@@ -1,16 +1,39 @@
-var canvas = document.getElementById('cells');
-var cv = canvas.getContext('2d');
-cv.beginPath();
-cv.moveTo(1,0);
-cv.lineTo(1,480);
-cv.moveTo(47,0);
-cv.lineTo(47,480);
-cv.moveTo(94,0);
-cv.lineTo(94,480);
-cv.moveTo(141,0);
-cv.lineTo(141,480);
-cv.moveTo(188,0);
-cv.lineTo(188,480);
-cv.strokeStyle = 'black';
-cv.stroke();
+var canvas = document.getElementById('cells'),
+	cv = canvas.getContext('2d'),
+	WIDTH = 420,
+	HEIGHT = 420,
+	render = () => {
+		cv.fillStyle = 'black';
+		cv.fillRect(0, 0, WIDTH, HEIGHT); // Clear the screen to redraw for this frame
+		cv.fillStyle = "white"
+		cv.fillRect(3, 3, WIDTH - 6, WIDTH - 6);
+		cv.strokeStyle = 'black';
+		for (let i = 1;i<9; i++) {
+			cv.beginPath();
+			if (i == 3 || i==6) {
+				cv.lineWidth = 3;
+			} else {
+				cv.lineWidth = 1;
+			}
+			cv.moveTo((WIDTH / 9) * i, 0);
+			cv.lineTo((WIDTH / 9) * i, 420);
+			cv.stroke();
+			cv.closePath();
+		}
+		for (let i = 0;i<9; i++) {
+			cv.beginPath();
+			if (i == 3 || i==6) {
+                                cv.lineWidth = 3;
+                        } else {
+                                cv.lineWidth = 1;
+                        }
+                        cv.moveTo(0, (HEIGHT / 9) * i);
+                        cv.lineTo(420, (HEIGHT / 9) * i);
+			cv.stroke();
+			cv.closePath();
+                }
 
+		cv.stroke();
+		requestAnimationFrame(render);
+	};
+requestAnimationFrame(render);
